dd2b071db8467bf27fe659864daf68526e9455c2
xiaoma <2536330053@qq.com>
2023-10-12 02:07:27 +0800

    访问nginx

--- src/util/requestForCopilotRemote.js
+++ src/util/requestForCopilotRemote.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export function requestForCopilotRemote(config) {
     const instance = axios.create({
         baseURL: 'https://copilot.tgmeng.com/copilotremote',
-        // baseURL: 'https://119.28.30.242:443/copilotremote',
         // baseURL: 'http://127.0.0.1:18686/copilotremote',
         timeout: 600000
     })
