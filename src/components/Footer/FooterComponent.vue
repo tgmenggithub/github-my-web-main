f27d2f8c5f5ef57cc7c67f68addae9adba2a92c0
xiaoma <2536330053@qq.com>
2023-11-13 16:55:29 +0800

    添加Copilot，去除midjourney

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -54,13 +54,14 @@
                     </div>
                     <div class="social" data-v-363e9324="">
                         <div data-v-363e9324="" class="footer-img-style">
-                            <a href="https://bot.tgmeng.com/drawer" target="_blank" data-v-363e9324="">
-                                <img src="../../assets/main/img/footer_img_midjourney.png" alt="Midjourney" data-v-363e9324="">
+                            <a href="https://shop.githubedu.com" target="_blank" data-v-363e9324="">
+                                <img src="../../assets/main/img/footer_img_copilot.png" alt="Copilot授权" data-v-363e9324="">
                             </a>
                         </div>
+
                         <div data-v-363e9324="" class="footer-img-style">
-                            <a href="https://bot.tgmeng.com" target="_blank" data-v-363e9324="">
-                                <img src="../../assets/main/img/footer_img_openai.png" alt="ChatGPT Plus" data-v-363e9324="">
+                            <a href="https://shop.githubedu.com" target="_blank" data-v-363e9324="">
+                                <img src="../../assets/main/img/footer_img_jetbrains.png" alt="Jetbrains全家桶" data-v-363e9324="">
                             </a>
                         </div>
                         <div data-v-363e9324="" class="footer-img-style">
@@ -69,8 +70,8 @@
                             </a>
                         </div>
                         <div data-v-363e9324="" class="footer-img-style">
-                            <a href="https://githubedu.com/jetbrains" target="_blank" data-v-363e9324="">
-                                <img src="../../assets/main/img/footer_img_jetbrains.png" alt="Jetbrains全家桶" data-v-363e9324="">
+                            <a href="https://bot.tgmeng.com" target="_blank" data-v-363e9324="">
+                                <img src="../../assets/main/img/footer_img_openai.png" alt="ChatGPT Plus" data-v-363e9324="">
                             </a>
                         </div>
                     </div>
